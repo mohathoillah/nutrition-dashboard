84af538dde2a1b8ccdb84497e4fb578b4da72b2f
mohathoillah <athok@ub.ac.id>
2026-06-29 23:35:23 +0900

    redo to original code

--- components/tables.py
+++ components/tables.py
@@ -35,9 +35,9 @@ def render_data_table(df, value_column, indicator_label, selected_year):
     csv = table_df.to_csv(index=False).encode("utf-8")
 
 # Disable the download button for now, as it may not be necessary for the current use case
-   # st.download_button(
-        label="Download filtered data as CSV",
-        data=csv,
-        file_name=f"{indicator_label.lower().replace(' ', '_')}_{selected_year}_filtered.csv",
-        mime="text/csv"
-    )
+#    st.download_button(
+#         label="Download filtered data as CSV",
+#         data=csv,
+#         file_name=f"{indicator_label.lower().replace(' ', '_')}_{selected_year}_filtered.csv",
+#         mime="text/csv"
+#     )
